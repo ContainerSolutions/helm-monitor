b173836cfe728be4ab9391178e745f729c8e81fe
Etienne Tremel <etienne.tremel@container-solutions.com>
2018-01-17 17:29:27 +0100

    Update installation script

--- install.sh
+++ install.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 current_version=$(sed -n -e 's/version:[ "]*\([^"]*\).*/\1/p' plugin.yaml)
 HELM_MONITOR_VERSION=${HELM_MONITOR_VERSION:-$current_version}
 
-file="${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-monitor"}/helm-monitor"
+dir=${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-monitor"}
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
-url="https://github.com/ContainerSolutions/helm-monitor/releases/download/v${HELM_MONITOR_VERSION}/helm-monitor_${os}_amd64"
+release_file="helm-monitor_${os}_${HELM_MONITOR_VERSION}.tgz"
+url="https://github.com/ContainerSolutions/helm-monitor/releases/download/v${HELM_MONITOR_VERSION}/${release_file}"
+
+mkdir -p $dir
 
 if command -v wget
 then
-  wget -O "${file}" "${url}"
+  wget -O ${dir}/${release_file} $url
 elif command -v curl; then
-  curl -o "${file}" "${url}"
+  curl -o ${dir}/${release_file} $url
 fi
 
-chmod +x "${file}"
+tar xf ${dir}/${release_file} -C $dir
+
+chmod +x ${dir}/helm-monitor
+
+rm ${dir}/${release_file}
